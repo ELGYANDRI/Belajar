b9cbbf9b009e9caf8bc3b576c734c5e613bb8cb3
husnialfarisal-tech <huhsnialfarisal@gmail.com>
2025-09-21 11:14:57 +0900

    Upload latihan2-pertemuan1

---
+++ bin/latihan2-pertemuan1.dart
@@ -0,0 +1,21 @@
+// Judul: Aplikasi Menghitung Luas dan Keliling Persegi Panjang
+// File: latihan2.dart
+
+void main() {
+  // Menyimpan panjang dan lebar dalam variabel bertipe double
+  double panjang = 10.0;
+  double lebar = 5.0;
+
+  // Menghitung luas persegi panjang
+  double luas = panjang * lebar;
+
+  // Menghitung keliling persegi panjang
+  double keliling = 2 * (panjang + lebar);
+
+  // Menampilkan hasil perhitungan
+  print("=== Menghitung Luas & Keliling Persegi Panjang ===");
+  print("Panjang  : $panjang");
+  print("Lebar    : $lebar");
+  print("Luas     : $luas");
+  print("Keliling : $keliling");
+}
